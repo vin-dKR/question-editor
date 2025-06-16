918bfcc0029c6cc4b12db035abcc9f3ac76e7e81
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-06-16 23:57:57 +0530

    ui: refined

--- src/components/QuestionEditor.jsx
+++ src/components/QuestionEditor.jsx
@@ -701,10 +701,10 @@ const QuestionEditor = ({ question, onUpdate, onToggleImageType, onUpdateAllQues
                 </div>
 
                 {/* Update Buttons */}
-                <div className="border-t pt-3 flex justify-between">
+                <div className="border-t pt-3 flex justify-end">
                     <button
                         className="px-4 py-1.5 bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-sm transition-all font-medium flex items-center gap-2 text-sm"
-                        onClick={() => onUpdate(question)}
+                        onClick={handleUpdateQuestion}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
